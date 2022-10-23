2322705ee8521eed0fcf3315731798805b4a42fd
EDY28 <junior.matias@unas.edu.pe>
2022-10-23 14:30:30 -0500

    se agrego

---
+++ src/Semana11/ejer05.java
@@ -0,0 +1,34 @@
+
+package Semana11;
+
+import java.util.Random;
+
+class metodo{
+    void llenar(int v[], int x){
+        Random rd=new Random();
+        for (int i = 0; i < x; i++) {
+            v[i]=rd.nextInt(5);
+        }
+    }
+    int sumar(int v[], int i){
+        if(i==0) return v[i];
+        else return v[i]+sumar(v,i-1);
+    }
+    void mostrar(int v[]){        
+        for(Integer i:v){
+            System.out.print(i.toString()+"\t");
+        }
+    }
+}
+public class ejer05 {
+    public static void main(String[] args) {
+        metodo m=new metodo();
+        int vector[]=new int[10];
+        m.llenar(vector, 10);
+        //System.out.println(vector[5]);
+        //System.out.println(vector[6]);
+        //System.out.println(vector[7]);
+        m.mostrar(vector);
+        System.out.println("\nLa suma es: "+m.sumar(vector, 9));
+    }
+}
